a7a9b9e5cf4bbad5055565c99e254ca599ada0e7
W Zhang <wz315@cam.ac.uk>
2021-02-20 22:52:40 +0900

    Mapping
    Can now display a real-time map of the arena, showing the position and bounding box of the robot. The Map class in mapping.py is derived from controller.Display. To update the map, call .update() in the main loop.
    
    Also fixed error with position and bearing calculation. Note that the simulation environment uses the relatively uncommon NUE coordinate system.

--- driver/robot.py
+++ driver/robot.py
@@ -1,4 +1,16 @@
+# Copyright (C) 2021 Weixuan Zhang
+#
+# SPDX-License-Identifier: MIT
+"""This module contains classes representing the robot.
+"""
+
+from math import atan2, pi
+
 from controller import Robot, GPS, Compass, Motor
+import numpy as np
+
+from utils import rotate_vector
+from mapping import Map
 
 
 class IDPCompass(Compass):
@@ -18,7 +30,7 @@ class IDPGPS(GPS):
 class IDPMotor(Motor):
     def __init__(self, name):
         super().__init__(name)
-        # self.setPosition(float('inf'))
+        self.setPosition(float('inf'))
         self.setVelocity(0.0)
 
 
@@ -26,11 +38,20 @@ class IDPRobot(Robot):
     def __init__(self):
         super().__init__()
 
+        """
+        length: Length of the robot, parallel to the axis running back-to-front, in meters
+        width: Width of the robot, perpendicular to the axis running back-to-front
+        """
+        self.length = 0.2
+        self.width = 0.1
+
         self.timestep = int(self.getBasicTimeStep())  # get the time step of the current world.
 
+        # Motors
         self.left_motor = self.getDevice('wheel1')
         self.right_motor = self.getDevice('wheel2')
 
+        # Sensors
         self.gps = self.getDevice('gps')  # or use createGPS() directly
         self.compass = self.getDevice('compass')
 
@@ -44,17 +65,86 @@ class IDPRobot(Robot):
     def createMotor(self, name: str) -> IDPMotor:
         return IDPMotor(name)
 
+    # The coordinate system is NUE, meaning positive-x is North etc
     @property
     def position(self) -> list:
-        return self.gps.getValues()
+        """The current position of the robot
+
+        Returns:
+            [float, float]: Position (East, North) in meters
+        """
+        pos = self.gps.getValues()
+        return [pos[2], pos[0]]  # (East, North) so extract z and x
 
     @property
     def speed(self) -> float:
+        """The current speed of the robot measured by the GPS
+
+        Returns:
+            float: Current speed (ms^-1)
+        """
         return self.gps.getSpeed()
 
     @property
     def bearing(self) -> float:
-        return self.compass.getValues()[2]
+        """The current bearing of the robot in radians
+
+        Returns:
+            float: Bearing measured clockwise from North, [0, 2pi]
+        """
+        north = self.compass.getValues()
+        rad = atan2(north[0], north[2])  # [-pi, pi]
+        return rad + 2 * pi if rad < 0 else rad
+
+    def get_bot_vertices(self):
+        """Get the coordinates of vertices of the bot in world frame (i.e. in meters)
+
+        The robot is assumed to be a rectangle.
+
+        Returns:
+            [np.ndarray]: List of coordinates
+        """
+        center = np.array(self.position)
+        angle = -self.bearing  # bearing is positive if clockwise while for rotation anticlockwise is positive
+        center_to_corner = np.array([self.width, self.length]) / 2
+
+        center_to_topleft = center_to_corner * np.array([-1, 1])
+        center_to_topright = center_to_corner
+        center_to_bottomright = center_to_corner * np.array([1, -1])
+        center_to_bottomleft = -center_to_corner
+
+        center_to_corners = [center_to_topleft, center_to_topright,
+                             center_to_bottomright, center_to_bottomleft]
+
+        vertices = [center + rotate_vector(v, angle) for v in center_to_corners]
+        return vertices
+
+    def get_bot_front(self, distance: float):
+        """Get the coordinates of a point a certain distance in front of the center of the robot
+
+        Args:
+            distance (float): Distance in front of the center
+
+        Returns:
+            np.ndarray: The coordinate
+        """
+
+        return np.array(self.position) + rotate_vector(np.array([0, distance]), -self.bearing)
+
+    def get_map(self, arena_length: float, name: str = 'map') -> Map:
+        """Get a map of the arena, on which the current position and bounding box of the robot
+        will be displayed.
+
+        This requires the robot to have a Display child node with name 'map'.
+
+        Args:
+            arena_length(float): Side length of the arena, which is assumed to be a square
+            name(str): Name of the Display node, default to 'map'
+
+        Returns:
+            Map: The map
+        """
+        return Map(self, arena_length, name)
 
     def drive_to_position(self, target):
         pass
