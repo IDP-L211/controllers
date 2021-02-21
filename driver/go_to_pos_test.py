2de16f40ffe3cdda32e98dc01631347a3576cecd
jasonb <jasonrbrown@virginmedia.com>
2021-02-21 18:15:56 +0000

    Add function to drive to position; add tests

---
+++ driver/go_to_pos_test.py
@@ -0,0 +1,20 @@
+# Test script for drive_to_pos
+
+from robot import IDPRobot
+
+# create the Robot instance.
+robot = IDPRobot()
+robot.max_motor_speed = 5
+robot.target_distance_threshold = 0.2
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+target_pos = [0.5, 0.5]
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    at_target = robot.drive_to_position(target_pos=target_pos)
+    print(at_target)
+    print(robot.position, robot.speed, robot.bearing)
