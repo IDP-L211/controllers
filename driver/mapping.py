c1e0b933ce1812c1b803f4175dd5ffe692dd194e
Weixuan Zhang <39925558+WeixuanZ@users.noreply.github.com>
2021-02-21 18:53:27 +0900

    Improving map and refactoring (#6)
    * Clearing Display before drawing
    
    * Some refactoring, also drawing results from ultrasonic sensor
    
    * Various fixes related to numpy types
    
    The controller (which we have no control of) does not accept numpy.int64 as an integer type, and .astype(int) converts the type to int64 instead of int. This caused problems when a np.ndarray is directly unpacked as arguments to functions (i.e. .toList() is not called).
    
    * Fixed dimension of results from matrix multiplication, returning [x, y] instead of [[x, y]]

--- driver/mapping.py
+++ driver/mapping.py
@@ -15,7 +15,9 @@ class Map(Display):
         super().__init__(name)
         self.robot = robot
         self.arena_length = arena_length
-        self.map_length = min(self.getWidth(), self.getHeight())
+        self.width = self.getWidth()
+        self.height = self.getHeight()
+        self.map_length = min(self.width, self.height)
 
     @staticmethod
     def coord_to_xylists(list_of_arrays: list) -> list:
@@ -35,36 +37,57 @@ class Map(Display):
         """
         return vec / arena_length * map_length
 
-    @staticmethod
-    def coordtransform_vector(vec: np.ndarray, arena_length, map_length) -> np.ndarray:
+    def coordtransform_world_to_map(self, vec: list) -> list:
         """Transfer the world coordinate (East, North) to image coordinate
 
         The image coordinate has (0,0) at the top left corner and (width-1,height-1) at
         the bottom right corner. Therefore, the North coordinate needs to be reversed.
         The origin also need to be shifted to the center.
+
+        Args:
+            vec(list): The coordinate in world frame
+
+        Returns:
+            [int, int]: The coordinate on the map, integer number of pixels
         """
-        return Map.convert_unit(vec, arena_length, map_length)[::-1] * np.array([1, -1]) \
-               + np.array([map_length] * 2) / 2
+        vec = np.array(vec)
+        vec_trans = (Map.convert_unit(vec, self.arena_length, self.map_length) * np.array([1, -1])
+                     + np.array([self.map_length] * 2) / 2)
+        # convert to integer, must use toList() to ensure type as int instead of np.int64
+        return vec_trans.astype(int).tolist()
 
     def get_map_bot_vertices(self) -> list:
-        vertices = self.robot.get_bot_vertices()
-        vertices_trans = [Map.coordtransform_vector(v, self.arena_length, self.map_length) for v in vertices]
+        # formatted as [[x1, x2, x3...], [y1, y2, y3...]]
+        return Map.coord_to_xylists(list(map(
+            self.coordtransform_world_to_map,
+            self.robot.get_bot_vertices()  # vertices in world frame
+        )))
 
-        return Map.coord_to_xylists(vertices_trans)
-
-    def get_map_bot_front(self, distance=None) -> list:
-        if distance is None:
+    def get_map_bot_front(self, distance: float = 0) -> list:
+        if distance <= 0:
             distance = self.robot.length / 0.8
-        vertices = [np.array([self.robot.position]), self.robot.get_bot_front(distance)]
-        vertices_trans = [Map.coordtransform_vector(v, self.arena_length, self.map_length).astype(int) for v in
-                          vertices]
 
-        return Map.coord_to_xylists(vertices_trans)
+        return self.coordtransform_world_to_map(self.robot.get_bot_front(distance))
 
-    def update(self, display_front: bool = True):
-        px, py = self.get_map_bot_vertices()
-        self.drawPolygon(px, py)
+    def draw_marker(self, map_coord: list) -> None:
+        # must be of type int not np.int64, pass in a list instead of np.ndarray
+        self.fillOval(*map_coord, 3, 3)
 
-        if display_front:
-            px, py = self.get_map_bot_front()
-            self.drawLine(px[0], py[0], px[1], py[1])
+    def draw_line_from_botcenter(self, map_coord: list) -> None:
+        # must be of type int not np.int64, pass in a list instead of np.ndarray
+        self.drawLine(*self.coordtransform_world_to_map(self.robot.position), *map_coord)
+
+    def update(self, draw_distance: bool = True) -> None:
+        # clear display
+        self.setColor(0x000000)
+        self.fillRectangle(0, 0, self.height, self.width)
+
+        # draw bounding box
+        self.setColor(0xFFFFFF)
+        self.drawPolygon(*self.get_map_bot_vertices())
+
+        # draw markers
+        front_coord = self.get_map_bot_front(self.robot.ultrasonic.getValue() if draw_distance else 0)
+        self.draw_line_from_botcenter(front_coord)
+        self.draw_marker(front_coord)
+        self.draw_marker(self.get_map_bot_front(self.robot.ultrasonic.max_range))
