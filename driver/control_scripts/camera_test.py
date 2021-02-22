e03882e43c0a11c25d261252614be69fb814692b
jasonb <jasonrbrown@virginmedia.com>
2021-02-22 21:38:35 +0000

    Improve execute_actions; refactor and restructure files

--- driver/control_scripts/camera_test.py
+++ driver/control_scripts/camera_test.py
@@ -1,25 +1,19 @@
-# Test script for drive_to_pos
-
+# Test script for camera, currently broken as robot.getDevice('camera') returns None
 
 def main(robot):
 
     # Setup
     timestep = int(robot.getBasicTimeStep())
+    camera = robot.getDevice('camera')
+    camera.enable(timestep)
     nav_map = robot.get_map(2.4)
-
-    # Actions for our robot
-    action_queue = [
-        ("move", [1, -1]),
-        ("move", [1, 1]),
-        ("face", 3.141),
-        ("move", [-1, 1]),
-        ("move", [-1, -1]),
-        ("move", [0, 0]),
-        ("face", 0.0)
-    ]
 
     # Main loop, perform simulation steps until Webots is stopping the controller
     while robot.step(timestep) != -1:
-        robot.execute_action(action_queue)
+
+        robot.left_motor.setVelocity(2.0)
+        robot.right_motor.setVelocity(1.0)
+
         print(robot.position, robot.speed, robot.bearing)
+        # print(camera.getImageArray())
         nav_map.update()
