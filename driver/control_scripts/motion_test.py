2369e30b5d67cea1f50d4b88e61e0b2f74b18d53
jasonb <jasonrbrown@virginmedia.com>
2021-02-22 22:27:55 +0000

    execute_action refactor fix

--- driver/control_scripts/motion_test.py
+++ driver/control_scripts/motion_test.py
@@ -20,6 +20,6 @@ def main(robot):
 
     # Main loop, perform simulation steps until Webots is stopping the controller
     while robot.step(timestep) != -1:
-        robot.execute_actions(action_queue)
+        robot.execute_action(action_queue)
         print(robot.position, robot.speed, robot.bearing)
         nav_map.update()
