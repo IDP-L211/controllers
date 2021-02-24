6c870700f482995a4417b9b6858a99a72efeae85
Jason Brown <68914161+jr-brown@users.noreply.github.com>
2021-02-24 19:33:25 +0000

    Add rotation action; improve motion code
    * Add rotation action; improve motion code
    
    * Add debug printing
    
    * Motion refactor part 2 electric boogaloo
    
    * Remove a legacy function
    
    Co-authored-by: W Zhang <wz315@cam.ac.uk>

--- driver/control_scripts/motion_test.py
+++ driver/control_scripts/motion_test.py
@@ -7,17 +7,18 @@ def main(robot):
 
     # Actions for our robot
     action_queue = [
+        ("rotate", 7),
         ("move", [1, -1]),
         ("move", [1, 1]),
         ("face", 3.141),
         ("move", [-1, 1]),
         ("move", [-1, -1]),
         ("move", [0, 0]),
-        ("face", 0.0)
+        ("face", 0.0),
+        ("rotate", -5)
     ]
 
     # Main loop, perform simulation steps until Webots is stopping the controller
     while robot.step(timestep) != -1:
         robot.execute_action(action_queue)
-        print(robot.position, robot.speed, robot.bearing)
         nav_map.update()
