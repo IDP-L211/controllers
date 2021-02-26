4aeeb5533daa510f2618864012c5f2066d3a7db0
Weixuan Zhang <39925558+WeixuanZ@users.noreply.github.com>
2021-02-27 03:54:28 +0900

    Geometry calculations (#14)
    * Get min distance between bots
    
    * Get min distance between vertices and walls
    
    * Get theoretical sensor distance to wall
    
    * Add mapping options and coordinate plotting method

--- driver/driver.py
+++ driver/driver.py
@@ -9,10 +9,12 @@ from robot import IDPRobot
 from control_scripts.motion_test import main as motion_test
 from control_scripts.camera_test import main as camera_test
 from control_scripts.object_processing_test import main as object_processing_test
+from control_scripts.sensor_dist_calc_test import main as sensor_dist_calc_test
 
 
 if __name__ == '__main__':
     robot = IDPRobot()
     # motion_test(robot)
     # camera_test(robot)
-    object_processing_test(robot)
+    # object_processing_test(robot)
+    sensor_dist_calc_test(robot)
