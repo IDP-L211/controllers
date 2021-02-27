6df3742cf4d21633bf7e6cdb15decb47d7392af9
Tim Clifford <tclifford@protonmail.com>
2021-02-27 00:09:01 +0000

    Add `IDPDistanceSensor.getBounds` and use scipy

--- driver/devices/sensors.py
+++ driver/devices/sensors.py
@@ -3,7 +3,11 @@
 # SPDX-License-Identifier: MIT
 """Sensors used on the robot"""
 from controller import GPS, Compass, DistanceSensor
+from scipy.interpolate import interp1d
+from scipy.optimize import root_scalar as root
+from warnings import warn
 
+DEBUG=True
 
 class IDPCompass(Compass):
     def __init__(self, name, sampling_rate):
@@ -22,38 +26,123 @@ class IDPDistanceSensor(DistanceSensor):
         self.decreasing = decreasing
 
         lookupTable = self.getLookupTable()
-        betterLookupTable = list(zip(*[
+        self.betterLookupTable = list(zip(*(
             lookupTable[i:i + 3] for i in range(0, len(lookupTable) - 2, 3)
             if lookupTable[i] >= min_range  # prevent it being many-to-one
-        ]))
+        )))
 
-        # For now we will ignore the uncertainty, but we should add it later
-        self.inverseLookupTable = betterLookupTable[1::-1]  # First two reversed
+        # Be VERY careful trying to refactor this
+        # Bound function at start and end values
+
+        # |  This helps you understand things, ok?
+        # |     -
+        # |     x
+        # |     -   -
+        # |   -     x   -
+        # |   x     -   x   -
+        # |             -   x     -
+        # |  -              -     x        -
+        # |  x                    -        x
+        # |  -                             -
+        # |--------------------------------
+
+        self.f_expectation = lambda x: interp1d(
+            *self.betterLookupTable[:2],
+            bounds_error=False,
+            fill_value=self.betterLookupTable[1][-1]
+        )(x) if x > self.min_range else self.betterLookupTable[1][0]
+
+        self.f_upper_bound = lambda x: interp1d(
+            self.betterLookupTable[0],
+            tuple(map(sum, zip(*self.betterLookupTable[1:]))),
+            bounds_error=False,
+            fill_value=tuple(
+                map(sum, zip(*self.betterLookupTable[1:]))
+            )[-1]
+        )(x) if x > self.min_range else tuple(
+            map(sum, zip(*self.betterLookupTable[1:]))
+        )[0]
+
+        self.f_lower_bound = lambda x: interp1d(
+            self.betterLookupTable[0],
+            tuple(map(lambda x: x[0] - x[1], zip(*self.betterLookupTable[1:]))),
+            bounds_error=False,
+            fill_value=tuple(
+                map(lambda x: x[0] - x[1], zip(*self.betterLookupTable[1:]))
+            )[-1]
+        )(x) if x > self.min_range else tuple(
+            map(lambda x: x[0] - x[1], zip(*self.betterLookupTable[1:]))
+        )[0]
+
 
     def getValue(self):
-        # We need some context about the response of the sensor in order to
-        # decode it properly
         v = super().getValue()
 
         # Don't attempt to interpolate if the value is outside the bounds
-        if (v > max(self.inverseLookupTable[0][0], self.inverseLookupTable[0][-1]) or
-                v < min(self.inverseLookupTable[0][0], self.inverseLookupTable[0][-1])):
+        if (v > max(self.betterLookupTable[1][0], self.betterLookupTable[1][-1]) or
+                v < min(self.betterLookupTable[1][0], self.betterLookupTable[1][-1])):
             return self.max_range
 
-        # interpolate the inverse lookup table
-        idx, x = next(filter(
-            lambda x: x[1] < v if self.decreasing else x[1] > v,
-            enumerate(self.inverseLookupTable[0])
-        ))
+        try:
+            sc_expectation_result = root(
+                lambda x: self.f_expectation(x) - v,
+                method = 'bisect',
+                bracket = sorted((
+                    self.betterLookupTable[0][0],
+                    self.betterLookupTable[0][-1],
+                )), x0 = (self.betterLookupTable[1][len(self.betterLookupTable)//2],),
+            )
+        except ValueError as e:
+            # Shouldn't happen
+            warn(f'WARNING: value out of bounds in {self.getName()}')
+            if "f(a) and f(b) must have different signs" in str(e):
+                return self.max_range
+            else: raise e
 
-        fractional_position = abs(
-            (self.inverseLookupTable[0][idx - 1] - v)
-            / (self.inverseLookupTable[0][idx - 1] - x)
-        )
-        return (
-                self.inverseLookupTable[1][idx - 1] + fractional_position
-                * (self.inverseLookupTable[1][idx] - self.inverseLookupTable[1][idx - 1])
-        )
+        if not sc_expectation_result.converged:
+            warn(f'WARNING: Could not calculate {self.getName()} value')
+            return self.max_range
+
+        # it shouldn't have more than one root
+        return sc_expectation_result.root
+
+
+    def getBounds(self):
+        v = super().getValue()
+
+        # Don't attempt to interpolate if the value is outside the bounds
+        try:
+            sc_bound_results = (
+                root(
+                    lambda x: self.f_lower_bound(x) - v,
+                    method = 'bisect',
+                    bracket = sorted((
+                        self.betterLookupTable[0][0],
+                        self.betterLookupTable[0][-1],
+                    )), x0 = (self.getValue(),)
+                ), root(
+                    lambda x: self.f_upper_bound(x) - v,
+                    method = 'bisect',
+                    bracket = sorted((
+                        self.betterLookupTable[0][0],
+                        self.betterLookupTable[0][-1],
+                    )), x0 = (self.getValue(),)
+                )
+            )
+        except ValueError as e:
+            # Doing the check ourselves here is actually not that simple
+            # because the function could be increasing or decreasing
+            # let's just rely on the ValueError
+            if "f(a) and f(b) must have different signs" in str(e):
+                return (self.max_range, 3.5) # Roughly the maximum possible
+            else: raise e
+
+        if any(map(lambda x: not x.converged, sc_bound_results)):
+            warn(f'WARNING: Could not calculate {self.getName()} bounds')
+            return (self.max_range, 3.5) # Roughly the maximum possible
+        else:
+            # it shouldn't have more than one root
+            return sorted(tuple(map(lambda y: y.root, sc_bound_results)))
 
 
 class IDPGPS(GPS):
--- driver/driver.py
+++ driver/driver.py
@@ -9,7 +9,7 @@ from robot import IDPRobot
 from control_scripts.motion_test import main as motion_test
 from control_scripts.camera_test import main as camera_test
 from control_scripts.object_processing_test import main as object_processing_test
-from control_scripts.sensor_dist_calc_test import main as sensor_dist_calc_test
+from control_scripts.sensor_bounds_test import main as sensor_bounds_test
 
 
 if __name__ == '__main__':
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     # motion_test(robot)
     # camera_test(robot)
     # object_processing_test(robot)
-    sensor_dist_calc_test(robot)
+    sensor_bounds_test(robot)
