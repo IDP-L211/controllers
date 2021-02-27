9e33d82305a727f2187c14d6e2a473c11635fe22
Eleanor Clifford <git@clifford.lol>
2021-02-27 18:39:26 +0000

    Add IDPDistanceSensor.getBounds and use scipy (#16)

---
+++ driver/control_scripts/sensor_bounds_ultrasonic_test.py
@@ -0,0 +1,15 @@
+def main(robot):
+    nav_map = robot.get_map(robot.ultrasonic)
+    action_queue = [
+        ("rotate", 6.28 * 2, 2)
+    ]
+
+    while robot.step(robot.timestep) != -1:
+        robot.execute_action(action_queue)
+
+        dis = robot.get_sensor_distance_to_wall()
+        print(*map(lambda x: f"{x:5f}", (
+            robot.ultrasonic.getValue(), *robot.ultrasonic.getBounds()
+        )))
+        nav_map.plot_coordinate(robot.get_bot_front(dis))
+        nav_map.update()
