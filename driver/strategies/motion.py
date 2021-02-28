590381101c191d8a50a54c8c7c49dd3f820b4cc1
jasonb <jasonrbrown@virginmedia.com>
2021-02-28 23:20:45 +0000

    Remove redundant motion control methods
    The PID controller can easily be configured to behave in a similar way
    to distance_angle_error or short_linear_region so they have been
    removed.
    
    Combining and scaling if over is better than simply capping since it
    preserves the ratio between forward and rotaional velocity.

--- driver/strategies/motion.py
+++ driver/strategies/motion.py
@@ -14,18 +14,6 @@ class MotionControlStrategies:
     """
 
     @staticmethod
-    def _combine_and_limit(forward, rotation, angle=None):
-        # Reverse rotation if angle is negative for symmetric strategies
-        if angle is not None:
-            rotation *= np.sign(angle)
-
-        # Make sure speeds are maxed at 1
-        left_speed = max(min(1, forward + rotation), -1)
-        right_speed = max(min(1, forward - rotation), -1)
-
-        return np.array([left_speed, right_speed])
-
-    @staticmethod
     def _combine_and_scale(forward, rotation, angle=None):
         # Reverse rotation if angle is negative for symmetric strategies
         if angle is not None:
@@ -76,7 +64,7 @@ class MotionControlStrategies:
             switch_angle (float): Within this angle from target use angle error, else rotational velocity, rad
 
         Returns:
-            np.array(float, float): The speed for the left and right motors respectively. Given as a fraction of max speed.
+            np.array(float, float): The speed for the left and right motors respectively. Fraction of max speed.
         """
 
         if current_distance is None and current_f_velocity is None:
@@ -98,97 +86,31 @@ class MotionControlStrategies:
         return MotionControlStrategies._combine_and_scale(forward_speed, rotation_speed)
 
     @staticmethod
-    def distance_angle_error(distance: float, angle: float, k_p_forward=4.0, k_p_rotation=4.0) -> np.array:
-        """Set wheel speeds based on angle and distance error
-
-        This method performs well for face_bearing but has issues with point to point travel - slow turn times and
-        slowing down a lot when near target
-
-        Args:
-            distance (float): Distance to target, m
-            angle (float): Angle to target, rad
-            k_p_forward (float): Value of k_p for forward speed
-            k_p_rotation (float): Value of k_p for rotation speed
-
-        Returns:
-            np.array(float, float): The speed for the left and right motors respectively. Given as a fraction of max speed.
-        """
-
-        forward_speed = distance * k_p_forward
-        rotation_speed = angle * k_p_rotation
-
-        # Attenuate and possibly reverse forward speed based on angle of bot - don't want to speed off away from it
-        forward_speed *= np.cos(angle)
-
-        # Combine speeds
-        speeds = np.array([forward_speed + rotation_speed, forward_speed - rotation_speed])
-
-        # This might be above our motor maximums so we'll use sigmoid to normalise our speeds to this range
-        # Sigmoid bounds -inf -> inf to 0 -> 1 so we'll need to do some correcting
-        speeds = (1 / (1 + np.exp(-speeds))) * 2 - 1
-
-        # Alternative to using sigmoid
-        # speeds = np.tanh(speeds)
-
-        # Another alternative where the minimum motor speed is half its value
-        # speeds = (1 / (1 + np.exp(-speeds))) + ((np.sign(speeds) - 1) * 0.5)
-
-        return speeds
-
-    @staticmethod
-    def angle_based_control(distance: float, angle: float, rotation_speed_profile_power=0.5,
-                            forward_speed_profile_power=3.0) -> np.array:
-        """Set fastest wheel speed to maximum with the other wheel slowed to facilitate turning
-
-        By using a cos^2 for our forward speed and sin^2 for our rotation speed, the fastest wheel will always be at max
-        drive fraction and the other wheel will correct for angle
-
-        This method seems to perform better for point to point travel but is slow to orientate the bot in face_bearing
+    def angle_based(distance: float, angle: float, r_speed_profile_power=0.5, f_speed_profile_power=3.0) -> np.array:
+        """Determine wheels speeds based on the current angle to target, designed to quickly turn to target and then go
+        at max forward speed. Could outperform PID control for when a robot doesn't need to stop in an exact spot.
 
         Args:
             distance (float): Distance from target, m. For this method only matters if it is 0 or not
             angle (float): Angle to target, rad
-            rotation_speed_profile_power (float): Exponent of rotation speed profile, [0, inf]
-            forward_speed_profile_power (float): How 'tight' to make the velocity profile, [0, inf]
+            r_speed_profile_power (float): Exponent of rotation speed profile, [0, inf]
+            f_speed_profile_power (float): How 'tight' to make the velocity profile, [0, inf]
 
         Returns:
-            np.array(float, float): The speed for the left and right motors respectively. Given as a fraction of max speed.
+            np.array(float, float): The speed for the left and right motors respectively. Fraction of max speed.
         """
         # For some reason (probably floating point errors), we occasionally get warnings about requested speed exceeding
         # max velocity even though they are equal. We shall subtract a small quantity to avoid this annoyance.
         small_speed = 1e-5
 
         # Forward speed calculation - aimed to be maximised when facing forward
-        forward_speed = (np.cos(angle)**forward_speed_profile_power) - small_speed if abs(angle) <= np.pi / 2 else 0
+        forward_speed = (np.cos(angle)**f_speed_profile_power) - small_speed if abs(angle) <= np.pi / 2 else 0
 
         # Use up the rest of our wheel speed for turning, attenuate to reduce aggressive turning
         rotation_speed = 1 - forward_speed
-        rotation_speed = ((abs(rotation_speed))/np.pi)**rotation_speed_profile_power
+        rotation_speed = ((abs(rotation_speed))/np.pi)**r_speed_profile_power
 
         # Zero forward speed if we're not actually needing to move forward
         forward_speed *= np.sign(distance)
 
-        return MotionControlStrategies._combine_and_limit(forward_speed, rotation_speed, angle)
-
-    @staticmethod
-    def short_linear_region(distance, angle, forward_drive=1, angle_drive=1,
-                            forward_lin_region_width=0.2, angular_lin_region_width=np.pi/10) -> np.array:
-        """Calculates wheel speeds based on trying to achieve a fixed drive but with a short linear region when close
-
-        Args:
-            distance (float): Distance from target, m. For this method only matters if it is 0 or not
-            angle (float): Angle to target, rad
-            forward_drive (float): Ideal forward drive when far from target [0, 1]
-            angle_drive (float): Ideal angle drive when far from target [0, 1]
-            forward_lin_region_width (float): Width of the linear region for forward drive
-            angular_lin_region_width (float): Width of the linear region for angular drive
-
-        Returns:
-            [float, float]: The speed for the left and right motors respectively. Given as a fraction of max speed."""
-
-        forward_speed = forward_drive if abs(distance) > forward_lin_region_width else\
-            (distance / forward_lin_region_width) * forward_drive
-        rotation_speed = angle_drive if abs(angle) > angular_lin_region_width else\
-            (angle / angular_lin_region_width) * angle_drive
-
-        return MotionControlStrategies._combine_and_limit(forward_speed, rotation_speed, angle)
+        return MotionControlStrategies._combine_and_scale(forward_speed, rotation_speed, angle)
